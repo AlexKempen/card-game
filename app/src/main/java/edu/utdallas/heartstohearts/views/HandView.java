23030dd4ad76ed0cec16545caa951da4e65e8820
Egan Johnson <egancjohnson@gmail.com>
2023-10-13 12:08:03 -0500

    Janky View Objects

--- app/src/main/java/edu/utdallas/heartstohearts/views/HandView.java
+++ app/src/main/java/edu/utdallas/heartstohearts/views/HandView.java
@@ -8,184 +8,89 @@ import android.graphics.Paint;
 import android.graphics.drawable.Drawable;
 import android.text.TextPaint;
 import android.util.AttributeSet;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.Button;
+import android.widget.TableLayout;
+import android.widget.TableRow;
+import android.widget.TextView;
+
+import java.util.EnumMap;
+import java.util.Map;
 
 import edu.utdallas.heartstohearts.R;
 
 /**
  * TODO: document your custom view class.
  */
-public class HandView extends View {
-    private String mExampleString; // TODO: use a default from R.string...
-    private int mExampleColor = Color.RED; // TODO: use a default from R.color...
-    private float mExampleDimension = 0; // TODO: use a default from R.dimen...
-    private Drawable mExampleDrawable;
 
-    private TextPaint mTextPaint;
-    private float mTextWidth;
-    private float mTextHeight;
+/**
+ * TODO: get rid of these suites, have single suite enum throughout project
+ */
+enum Suites{
+    Hearts,
+    Spades,
+    Diamonds,
+    Clubs,
+}
+
+public class HandView extends TableLayout {
+
+    private EnumMap<Suites, TableRow> suite_rows;
 
     public HandView(Context context) {
         super(context);
-        init(null, 0);
+        init(context);
     }
 
     public HandView(Context context, AttributeSet attrs) {
         super(context, attrs);
-        init(attrs, 0);
+        init(context);
     }
 
-    public HandView(Context context, AttributeSet attrs, int defStyle) {
-        super(context, attrs, defStyle);
-        init(attrs, defStyle);
-    }
 
-    private void init(AttributeSet attrs, int defStyle) {
-        // Load attributes
-        final TypedArray a = getContext().obtainStyledAttributes(
-                attrs, R.styleable.HandView, defStyle, 0);
+    protected void init(Context context){
+        // Load all elements from the XML into this view
+        LayoutInflater.from(context).inflate(R.layout.hand_view, this);
+        suite_rows = new EnumMap<Suites, TableRow>(Suites.class);
+        suite_rows.put(Suites.Hearts, this.findViewById(R.id.heartsRow));
 
-        mExampleString = a.getString(
-                R.styleable.HandView_exampleString);
-        mExampleColor = a.getColor(
-                R.styleable.HandView_exampleColor,
-                mExampleColor);
-        // Use getDimensionPixelSize or getDimensionPixelOffset when dealing with
-        // values that should fall on pixel boundaries.
-        mExampleDimension = a.getDimension(
-                R.styleable.HandView_exampleDimension,
-                mExampleDimension);
+        addCard("Dynamic card");
 
-        if (a.hasValue(R.styleable.HandView_exampleDrawable)) {
-            mExampleDrawable = a.getDrawable(
-                    R.styleable.HandView_exampleDrawable);
-            mExampleDrawable.setCallback(this);
-        }
-
-        a.recycle();
-
-        // Set up a default TextPaint object
-        mTextPaint = new TextPaint();
-        mTextPaint.setFlags(Paint.ANTI_ALIAS_FLAG);
-        mTextPaint.setTextAlign(Paint.Align.LEFT);
-
-        // Update TextPaint and text measurements from attributes
-        invalidateTextPaintAndMeasurements();
-    }
-
-    private void invalidateTextPaintAndMeasurements() {
-        mTextPaint.setTextSize(mExampleDimension);
-        mTextPaint.setColor(mExampleColor);
-        mTextWidth = mTextPaint.measureText(mExampleString);
-
-        Paint.FontMetrics fontMetrics = mTextPaint.getFontMetrics();
-        mTextHeight = fontMetrics.bottom;
+        invalidate();
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
-
-        // TODO: consider storing these as member variables to reduce
-        // allocations per draw cycle.
-        int paddingLeft = getPaddingLeft();
-        int paddingTop = getPaddingTop();
-        int paddingRight = getPaddingRight();
-        int paddingBottom = getPaddingBottom();
-
-        int contentWidth = getWidth() - paddingLeft - paddingRight;
-        int contentHeight = getHeight() - paddingTop - paddingBottom;
-
-        // Draw the text.
-        canvas.drawText(mExampleString,
-                paddingLeft + (contentWidth - mTextWidth) / 2,
-                paddingTop + (contentHeight + mTextHeight) / 2,
-                mTextPaint);
-
-        // Draw the example drawable on top of the text.
-        if (mExampleDrawable != null) {
-            mExampleDrawable.setBounds(paddingLeft, paddingTop,
-                    paddingLeft + contentWidth, paddingTop + contentHeight);
-            mExampleDrawable.draw(canvas);
-        }
     }
 
     /**
-     * Gets the example string attribute value.
+     * TODO make this do what its supposed to, not just add text
+     * @param card_name
      *
-     * @return The example string attribute value.
+     * @return id of the added card
      */
-    public String getExampleString() {
-        return mExampleString;
+    public int addCard(String card_name){
+        TableRow row = suite_rows.get(Suites.Hearts);
+        Button button = new Button(getContext());
+        button.setText(card_name);
+        row.addView(button);
+        row.invalidate();
+
+        // TODO get rid of this part:
+        button.setOnClickListener((View clicked)-> {
+            removeCard(button.getId());
+        });
+
+        return button.getId();
     }
 
-    /**
-     * Sets the view"s example string attribute value. In the example view, this string
-     * is the text to draw.
-     *
-     * @param exampleString The example string attribute value to use.
-     */
-    public void setExampleString(String exampleString) {
-        mExampleString = exampleString;
-        invalidateTextPaintAndMeasurements();
+    public void removeCard(int card_id){
+        // TODO: safety. What if wrong card id?
+        TableRow row = (TableRow) findViewById(card_id).getParent();
+        row.removeView(findViewById(card_id));
+        row.invalidate();
     }
 
-    /**
-     * Gets the example color attribute value.
-     *
-     * @return The example color attribute value.
-     */
-    public int getExampleColor() {
-        return mExampleColor;
-    }
-
-    /**
-     * Sets the view"s example color attribute value. In the example view, this color
-     * is the font color.
-     *
-     * @param exampleColor The example color attribute value to use.
-     */
-    public void setExampleColor(int exampleColor) {
-        mExampleColor = exampleColor;
-        invalidateTextPaintAndMeasurements();
-    }
-
-    /**
-     * Gets the example dimension attribute value.
-     *
-     * @return The example dimension attribute value.
-     */
-    public float getExampleDimension() {
-        return mExampleDimension;
-    }
-
-    /**
-     * Sets the view"s example dimension attribute value. In the example view, this dimension
-     * is the font size.
-     *
-     * @param exampleDimension The example dimension attribute value to use.
-     */
-    public void setExampleDimension(float exampleDimension) {
-        mExampleDimension = exampleDimension;
-        invalidateTextPaintAndMeasurements();
-    }
-
-    /**
-     * Gets the example drawable attribute value.
-     *
-     * @return The example drawable attribute value.
-     */
-    public Drawable getExampleDrawable() {
-        return mExampleDrawable;
-    }
-
-    /**
-     * Sets the view"s example drawable attribute value. In the example view, this drawable is
-     * drawn above the text.
-     *
-     * @param exampleDrawable The example drawable attribute value to use.
-     */
-    public void setExampleDrawable(Drawable exampleDrawable) {
-        mExampleDrawable = exampleDrawable;
-    }
 }
