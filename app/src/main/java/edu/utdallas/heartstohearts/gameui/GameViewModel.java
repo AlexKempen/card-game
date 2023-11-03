a48c274a1f49f1b36b4a8e7c0ffeff78ff132ddf
Alex Kempen <alex.bookreader@gmail.com>
2023-11-03 11:40:50 -0500

    Removed code in GameViewModel

--- app/src/main/java/edu/utdallas/heartstohearts/gameui/GameViewModel.java
+++ app/src/main/java/edu/utdallas/heartstohearts/gameui/GameViewModel.java
@@ -4,16 +4,15 @@ import android.util.Log;
 
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
-import androidx.lifecycle.SavedStateHandle;
 import androidx.lifecycle.SavedStateHandleSupport;
 import androidx.lifecycle.ViewModel;
 import androidx.lifecycle.viewmodel.ViewModelInitializer;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import edu.utdallas.heartstohearts.game.Card;
-import edu.utdallas.heartstohearts.game.Scores;
+import edu.utdallas.heartstohearts.game.GameState;
+import edu.utdallas.heartstohearts.game.GameStateBuilder;
 
 public class GameViewModel extends ViewModel {
     static final ViewModelInitializer<GameViewModel> initializer = new ViewModelInitializer<>(GameViewModel.class, creationExtras -> {
@@ -21,7 +20,7 @@ public class GameViewModel extends ViewModel {
         GameActivity gameActivity = (GameActivity) creationExtras.get(SavedStateHandleSupport.VIEW_MODEL_STORE_OWNER_KEY);
         String socketPort = (String) gameActivity.getIntent().getExtras().get("socket");
         Log.d(GameActivity.TAG, "View model init port: " + socketPort);
-        return new GameViewModel(new GameState());
+        return new GameViewModel(new GameStateBuilder().make().get(0));
     });
     private final MutableLiveData<GameState> gameStateData;
 
@@ -33,40 +32,22 @@ public class GameViewModel extends ViewModel {
         return gameStateData;
     }
 
-    public void setTrick(List<Card> trick) {
-        GameState currState = gameStateData.getValue();
-        currState.setTrick(trick);
-        gameStateData.setValue(currState);
-    }
-
-    public void setHand(List<Card> hand) {
-        GameState currState = gameStateData.getValue();
-        currState.setHand(hand);
-        gameStateData.setValue(currState);
-    }
-
-    public void setScores(Scores scores) {
-        GameState currState = gameStateData.getValue();
-        currState.setScores(scores);
-        gameStateData.setValue(currState);
-    }
-
     /**
      * Choose three cards to pass.
      */
     public void passCards(List<Card> cards) {
-        GameState currState = gameStateData.getValue();
-        currState.getHand().removeAll(cards);
-        gameStateData.setValue(currState);
+//        GameState currState = gameStateData.getValue();
+//        currState.getHand().removeAll(cards);
+//        gameStateData.setValue(currState);
     }
 
     /**
      * Choose a card to play from your hand.
      */
     public void playCard(Card card) {
-        GameState currState = gameStateData.getValue();
-        currState.getHand().remove(card);
-        currState.getTrick().add(card);
-        gameStateData.setValue(currState);
+//        GameState currState = gameStateData.getValue();
+//        currState.getHand().remove(card);
+//        currState.getTrick().add(card);
+//        gameStateData.setValue(currState);
     }
 }
